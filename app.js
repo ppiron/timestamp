e61a72b63c1ffca5e17c3e1da67ae7c257cc8a50
Paolo Pironi <ppiron@gmail.com>
2018-02-26 14:47:23 +0000

    minor changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.port || 3000;
 app.get('/:id', function (req, res, next) {
     console.log(req.get('accept-language').split(',')[0]);
     console.log(req.get('user-agent').match(/\(.*?(?=\))/)[0].slice(1));
+    console.log(req.connection.remoteAddress);
     const response = {};
     let param = req.params.id;
     if (parseInt(param, 10)) {
