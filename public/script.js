603e3284c6c10f8afc884e1b3f6d26a3fe49d474
Paolo <ppiron@gmail.com>
2018-03-08 18:42:09 +0000

    window.location.assign()

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const input = document.querySelector('#time-input');
-let url = "http://localhost:3000/";
 const navigate = (event) => {
     if (event.keyCode === 13) {
-        console.log(encodeURI(event.target.value));
-        url = url + event.target.value;
-        window.location.href = encodeURI(url);
+        window.location.assign(event.target.value);
     }
 }
